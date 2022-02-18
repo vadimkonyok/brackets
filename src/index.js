daf1893453291ea63624d2a49f545cdbc33820da
vadimkonyok <vadim.konyok@mail.ru>
2022-02-18 10:24:47 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arrayBrackets = bracketsConfig.map((element) => element.join(""));
+
+  for (let i = 0; i < arrayBrackets.length; i++) {
+    if (str.includes(arrayBrackets[i])) {
+      str = str.replace(arrayBrackets[i], "");
+      i = -1;
+    }
+  }
+
+  return !str.length;
+};
